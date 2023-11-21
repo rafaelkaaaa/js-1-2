df85905983d385468ba128a24d7cb48217d52a35
rafaelkaaaa <147595141+rafaelkaaaa@users.noreply.github.com>
2023-11-21 13:27:35 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+
+
+var name
+let age
+const PI = 3.14
+name = "Rafael";
+age = 17;
+let sity = "Almaty";
+age = 18;
+let x = 5;
+let y = 3;
+let xxx = x - y;
+
+console.log(xxx);
+
+let firstName = 'Rafael';
+let lastName = 'Gatiyatulin';
+
+console.log(firstName +' '+ lastName)
+
+
+let a = 1;
+let b = 2;
+let c = 3;
+let sum = a + b;
+let sum2 = a - b;
+let sum3 = a * b;
+let sum4 = a / b;
+let sum5 = a % b;
+
+console.log(sum)
+console.log(sum2)
+console.log(sum3)
+console.log(sum4)
+console.log(sum5)
+
+
+let isTrue = 'True';
+let isFalse = 'False';
+let sum6 = isTrue && isFalse;
+let sum7 = isTrue || isFalse;
+let sum8 =  !isTrue;
+
+
+console.log(sum6)
+console.log(sum7)
+console.log(sum8)
+
+let counter = 0;
+counter++;
+counter++;
+counter++;
+counter++;
+counter++;
+counter++;
+counter++;
+
+console.log(counter)
+counter--;
+console.log(counter)
+
+let greeting = 'Привет';
+//let name = 'Raf';//
+let message =
+
+alert(greeting +' '+name)
+
+const elementByld = document.querySelector("#id1");
+console.log(elementByld)
+const elementId = document.querySelector(".class");
+console.log(elementId)
+const elementTag = document.querySelector(h2);
+console.log(elementTag)
+
+
+const elm = document.querySelector('.div');
+const elementInsideElement = elm.querySelector('h3');
+
+elementByld.textContent = "Новый текст";
+
+elementByld.setAttribute("id","id2")
+
+elementByld.stylecolor = "blue";
+elementByld.style.backgroundcolor = "lightgray";
+
+elementByld.classList.add("new-class");
+elementByld.classList.remove("old-class");
+
